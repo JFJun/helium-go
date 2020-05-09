94c656d18e53fb565ff42dbbd9ebc2e3ccfb037d
JFJun <fujun.jiang@rylink.com>
2020-05-09 18:51:08 +0800

    add helium tx proto

--- test/sign_test.go
+++ test/sign_test.go
@@ -63,5 +63,68 @@ func TestCreateAddress(t *testing.T) {
 	fmt.Println(hex.EncodeToString(pub2))
 	fmt.Println(pub2)
 	fmt.Println(kp2.CreateAddress(pub2))
+	pub := []byte{156, 101, 157, 114, 60, 193, 232, 16,
+		167, 46, 120, 247, 222, 175, 71, 54,
+		168, 127, 16, 239, 143, 207, 200, 1,
+		0, 181, 51, 39, 231, 238, 73, 164}
+	fmt.Println(hex.EncodeToString(pub))
+	kp := crypto.New(1)
+	address := kp.CreateAddress(pub)
+	fmt.Println(address)
 
+}
+
+func Test_Sign(t *testing.T) {
+	alice := "148d8KTRcKA5JKPekBcKFd4KfvprvFRpjGtivhtmRmnZ8MFYnP3"
+	bob := "13M8dUbxymE3xtiAXszRkGMmezMhBS8Li7wEsMojLdb4Sdxc4wc"
+	paymentV1, err := transactions.BuildPaymentV1Tx(bob, alice, 10, 0, 1, nil)
+	fmt.Println("paymentV1:  ", paymentV1)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	kp := crypto.New(1)
+	seed, _ := hex.DecodeString("72eb1995e90e8b7c0054dcf594f4822572eb1995e90e8b7c0054dcf594f48225")
+	kp.PrivateKey = seed
+	sig := kp.PrivateKey.Sign(1, paymentV1)
+	sign_paymentV1, _ := transactions.BuildPaymentV1Tx(bob, alice, 10, 1, 1, sig)
+	fmt.Println(base64.StdEncoding.EncodeToString(sign_paymentV1))
+
+}
+
+func Test_Paymentv1(t *testing.T) {
+	alice := "148d8KTRcKA5JKPekBcKFd4KfvprvFRpjGtivhtmRmnZ8MFYnP3"
+	bob := "13M8dUbxymE3xtiAXszRkGMmezMhBS8Li7wEsMojLdb4Sdxc4wc"
+
+	paymentV1, err := transactions.BuildPaymentV1Tx(bob, alice, 10, 0, 1, []byte("some signature"))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("paymentV1:  ", paymentV1)
+
+	fmt.Println(len(paymentV1))
+	//50 14 115 111 109 101 32 115 105 103 110 97 116 117 114 101
+	//bob:=[]byte("abcdefg")
+	//fmt.Println(bob)
+	//alice:=[]byte("123456")
+	//fmt.Println(alice)
+	//sig:=[]byte("test")
+	//fmt.Println(sig)
+	//bob:=[]byte{61,62,63,64,65,66,67}
+	//alice:=[]byte{31,32,33,34,35,36}
+	//sig:=[]byte{74,65,73,74}
+	//v1:=&protos.BlockchainTxnPaymentV1{
+	//	Payer:     bob,
+	//	Payee:     alice,
+	//	Amount:    10,
+	//	Fee:       1,
+	//	Nonce:     2,
+	//	Signature: sig,
+	//}
+	//payment,err:=proto.Marshal(v1)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//fmt.Println(payment)
+	//fmt.Println(len(payment))
 }
--- transactions/transaction.go
+++ transactions/transaction.go
@@ -1,16 +1,31 @@
 package transactions
 
 import (
+	"encoding/hex"
+	"fmt"
 	"github.com/JFJun/helium-go/protos"
+	"github.com/btcsuite/btcutil/base58"
 	"github.com/golang/protobuf/proto"
 )
 
 //构建交易
 
 func BuildPaymentV1Tx(from, to string, amount, fee, nonce uint64, sig []byte) ([]byte, error) {
-	payer := []byte(from)
-	payee := []byte(to)
-	v1 := protos.PaymentV1{
+
+	fromB := base58.Decode(from)
+	payer := make([]byte, 33)
+	copy(payer, fromB[1:len(fromB)-4])
+	toB := base58.Decode(to)
+	payee := make([]byte, 33)
+	copy(payee, toB[1:len(toB)-4])
+
+	//pay1,_:=base64.StdEncoding.DecodeString("ATEzTThkVWJ4eW1FM3h0aUFYc3pSa0dNbWV6TWhCUzhMaTd3RXNNb2pMZGI0U2R4YzR3Yw==")
+	//pee1,_:=base64.StdEncoding.DecodeString("ATE0OGQ4S1RSY0tBNUpLUGVrQmNLRmQ0S2Z2cHJ2RlJwakd0aXZodG1SbW5aOE1GWW5QMw==")
+	//fmt.Println(pay1)
+	//fmt.Println(len(pay1))
+	//fmt.Println(pee1)
+	//fmt.Println(len(pee1))
+	v1 := &protos.BlockchainTxnPaymentV1{
 		Payer:     payer,
 		Payee:     payee,
 		Amount:    amount,
@@ -18,33 +33,20 @@ func BuildPaymentV1Tx(from, to string, amount, fee, nonce uint64, sig []byte) ([
 		Nonce:     nonce,
 		Signature: nil,
 	}
-	if sig != nil {
-		v1.Signature = sig
-	}
 
-	return proto.Marshal(&v1)
-}
+	//if sig != nil {
+	//	v1.Signature = sig
+	//}
+	s, _ := hex.DecodeString("0a2101351a71c22fefec2231936ad2826b217ece39d9f77fc6c49639926299c38692951221019c659d723cc1e810a72e78f7deaf4736a87f10ef8fcfc80100b53327e7ee49a4180a2801")
+	fmt.Println(s)
+	fmt.Println(len(s))
+	ss, _ := proto.Marshal(v1)
+	fmt.Println(ss)
+	fmt.Println(len(ss))
 
-func BuildPaymentV2Tx(from string, to_amount map[string]uint64, fee, nonce uint64, sig []byte) ([]byte, error) {
-	payer := []byte(from)
-	var payments []*protos.Payment
-	for to, amount := range to_amount {
-		payee := []byte(to)
-		payment := &protos.Payment{
-			Payee:  payee,
-			Amount: amount,
-		}
-		payments = append(payments, payment)
-	}
-	v2 := protos.PaymentV2{
-		Payer:     payer,
-		Payments:  payments,
-		Fee:       fee,
-		Nonce:     nonce,
-		Signature: nil,
-	}
-	if sig != nil {
-		v2.Signature = sig
-	}
-	return proto.Marshal(&v2)
+	txnPV1 := &protos.BlockchainTxn_Payment{Payment: v1}
+	txn := &protos.BlockchainTxn{}
+	txn.SetBlockTxn(txnPV1)
+
+	return proto.Marshal(txn)
 }
